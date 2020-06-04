44b56351b68ca801c4c8fb05e628c551e32d6d9e
mousam sharma <mousam.sharma13@gmail.com>
2020-06-04 21:39:14 +0530

    make it better

--- src/app/dashboard/dash-content/dash-content.component.ts
+++ src/app/dashboard/dash-content/dash-content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
+import { FormGroup, FormBuilder, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dash-content',
@@ -7,12 +8,36 @@ import { DataService } from 'src/app/services/data.service';
   styleUrls: ['./dash-content.component.css']
 })
 export class DashContentComponent implements OnInit {
-
+  myMatForm:FormGroup;
   MainArr=[]
 
-  constructor(private data: DataService) { }
+  constructor(private _formBuilder: FormBuilder,private data: DataService) { }
+
+  
+  onSubmit(myform : NgForm) {
+    console.log("form value :: ",this.myMatForm.value);
+    // let oj = { name : this.myMatForm.value.name,number : this.myMatForm.value.number};
+    // this.MainArr.push(this.myMatForm.value)
+    console.log(this.MainArr)
+    // this.newMessage()
+  // this.myMatForm.reset();
+  }
+
+  delete(i:any){
+    console.log(i)
+    this.MainArr.splice(i,1);
+  }
+
+  update(i:any){
+console.log("UP DATA : ",this.MainArr[i].name,this.MainArr[i].email)
+this.myMatForm.setValue({name:this.MainArr[i].name,email:this.MainArr[i].email})
+  }
 
   ngOnInit(): void {
+    this.myMatForm= this._formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    });
     this.data.currentMessage.subscribe(message => this.MainArr = message)
   }
 
